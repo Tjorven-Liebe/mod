1873ae10e4f1e5a3f3a4d70d938dfeacabecb203
JonasDevDE <73913242+JonasDevDE@users.noreply.github.com>
2025-07-23 01:35:31 +0200

    chore: Cleaned up tjorvens very dirty code

--- forge/src/main/java/net/eternalempires/mod/forge/ClientModEvents.java
+++ forge/src/main/java/net/eternalempires/mod/forge/ClientModEvents.java
@@ -61,6 +61,7 @@ public final class ClientModEvents {
         final Injector injector = EternalEmpiresClient.init();
 
         commonService = injector.getInstance(CommonService.class);
+        
         final PacketHandler packetHandler = injector.getInstance(PacketHandler.class);
 
         event.enqueueWork(packetHandler::register);
